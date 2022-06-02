ae9a905f6da5bd1e0ac7f38d5404ec1b221c6180
Victoria Solís <dual.victoria.solis@gmail.com>
2022-06-02 16:52:30 -0500

    Creación de entidades

---
+++ Entities/IncomeOutputEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities
+{
+    public class IncomeOutputEntity
+    {
+        [Key]
+        [StringLength(50)]
+        public string IncomeOutputId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string IncomeOutputName { get; set; }
+
+        public int IncomeQuantity { get; set; }
+        public int OutputQuantity { get; set; }
+        public int TotalQuantity { get; set; }
+    }
+}
---
+++ Entities/StorageEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities
+{
+    public class StorageEntity
+    {
+        [Key]
+        [StringLength(50)]
+        public string StorageId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string StorageName { get; set; }
+
+        public int TotalQuantity { get; set; }
+    }
+}
