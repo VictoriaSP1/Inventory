4269b4318c4f876605537b0434ac68e95ea391bb
Victoria Solís <dual.victoria.solis@gmail.com>
2022-06-03 10:10:25 -0500

    Creación del DataContext para las entidades

---
+++ DataAccess/InventaryContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class InventaryContext : DbContext
+    {
+        public DbSet <ProductEntity> Product { get; set; }
+        public DbSet <CategoryEntity> Category { get; set; }
+        public DbSet <InputOutputEntity> InOut { get; set; }
+        public DbSet <WarehouseEntity> Warehouse { get; set; }
+        public DbSet <StorageEntity> Storage { get; set; }
+
+
+    }
+}
